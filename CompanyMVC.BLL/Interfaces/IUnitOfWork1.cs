ce01876caebf0348f18460a10ad18bc83754f389
Shaimaa-Sabry1 <shaimaasabry129@gmail.com>
2025-06-22 14:02:04 +0200

    modify

---
+++ CompanyMVC.BLL/Interfaces/IUnitOfWork1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyMVC.BLL.Interfaces
+{
+    public interface IUnitOfWork1
+    {
+    }
+}
